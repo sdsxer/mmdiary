f213fe6544a0b07af441dfaf9e558bbb261d02cb
liang <liang@le.com>
2017-11-06 10:14:05 +0800

    fix mock mvc context path mapping error.
    Signed-off-by: liang <liang@le.com>

--- server/source/mmdiary/src/test/java/com/sdsxer/mmdiary/web/UserControllerTest.java
+++ server/source/mmdiary/src/test/java/com/sdsxer/mmdiary/web/UserControllerTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -32,6 +31,7 @@ public class UserControllerTest {
   @Test
   public void login() throws Exception {
       mvc.perform(MockMvcRequestBuilders.post("/mmdiary/user/login")
+            .contextPath("/mmdiary")
             .contentType(MediaType.APPLICATION_FORM_URLENCODED)
             .accept(MediaType.APPLICATION_JSON)
             .param("mobile", "18810789600")
